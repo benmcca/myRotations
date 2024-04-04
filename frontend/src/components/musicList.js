4724b1e631177c9eea35e5bd338ddde28460eac5
Ben McCabe <benmccabe19@gmail.com>
2024-04-03 22:07:43 -0400

    Added in new route for getting song by id

--- frontend/src/components/musicList.js
+++ frontend/src/components/musicList.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import MusicDataService from "../services/musicDataService"
+import { Link } from "react-router-dom"
+import musicDataService from '../services/musicDataService';
 
-function MusicList() {
+const MusicList = () => {
+  const [music, setMusic] = useState([]);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchArtist, setSearchArtist] = useState("");
+  useEffect(() => {
+    retrieveMusic();
+  }, []);
+
+
+  const retrieveMusic = () => {
+    musicDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setMusic(response.data.music);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value
+    setSearchTitle(searchTitle);
+  };
+
+  const onChangeSearchArtist = (e) => {
+    const searchArtist = e.target.value;
+    setSearchArtist(searchArtist);
+  };
+
     return (
         <div className="App">
             Music List
--- frontend/src/services/musicDataService.js
+++ frontend/src/services/musicDataService.js
@@ -5,41 +5,37 @@ class MusicDataService {
 
     getAll(page = 0) {
       return axios.get(
-        `${process.env.REACT_APP_BACKEND_URL}/api/v1/music?page=${page}`
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/music?page=${page}`
       );
     }
 
     get(id) {
       return axios.get(
-        `${process.env.REACT_APP_BACKEND_URL}/api/v1/music/id/${id}`
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/music/id/${id}`
       );
     }
 
     find(query, by = "trackName", page = 0) {
         return axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/api/v1/movies?${by}=${query}&page=${page}`
+          `${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/movies?${by}=${query}&page=${page}`
         );
     }
     
     createComment(data) {
-        return axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/music/comments`, data)
+        return axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/music/comments`, data)
     }
 
     updateComment(data) {
-    return axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/v1/music/comments`, data)
+    return axios.put(`${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/music/comments`, data)
     }
 
     deleteReview(id, userId) {
         return axios.delete(
-          `${process.env.REACT_APP_BACKEND_URL}/api/v1/music/comments`,
+          `${process.env.REACT_APP_BACKEND_URL}/api/v1/bsm25/music/comments`,
           { data: { commentId: id, userId: userId } }
         )
       }
     
-    getRatings() {
-    return axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/movies/ratings`)
-
-    }
 }
 export default new MusicDataService();
     
